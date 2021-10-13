56d03a0a2f37bac07f87d0579bfbd36da5acf305
TN57-BotZ <90815719+TN57-BotZ@users.noreply.github.com>
2021-10-13 20:07:10 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -154,7 +154,7 @@ def get_readable_message():
                            f" | <b>➜ Leechers :</b> <code>{download.torrent_info().num_leechs}</code>"
                 except:
                     pass
-                msg += f"\n<b>➜ To Cancel :</b> <code>/{BotCommands.CancelMirror}@TN57_Mirror3_Bot {download.gid()}</code>\n\n══════════════════════════════════════════"
+                msg += f"\n<b>➜ To Cancel :</b> <code>/{BotCommands.CancelMirror}@TN57_Mirror1_Bot {download.gid()}</code>\n\n══════════════════════════════════════════"
             msg += "\n\n"
             if STATUS_LIMIT is not None and index == STATUS_LIMIT:
                 break
